c233b7f1bc61b6ca921417fcb38c6b321b3acffb
Yang Fan <jane.fan1002@gmail.com>
2018-10-19 15:06:56 -0500

    Add mongoose schemas for user and recipe.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 require("dotenv").config({ path: "variables.env" });
+// const Recipe = require("./models/Recipe");
+// const User = require("./models/User");
 
+// connects to databas
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("DB connect."))
   .catch(err => console.error(err));
 
+// initialize
 const app = express();
 
 const PORT = process.env.PORT || 4444;
