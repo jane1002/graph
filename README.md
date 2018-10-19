fdca47a540b7b85334c61924123a044cce339d61
Yang Fan <jane.fan1002@gmail.com>
2018-10-19 14:44:47 -0500

    Add eslint fix command.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 GraphQL_Apollo_React-example
 Udemy course: https://www.udemy.com/full-stack-react-with-graphql-and-apollo-boost/learn/v4/overview
 Start from : https://github.com/reedbarger/react-recipes 
+
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 
 require("dotenv").config({ path: "variables.env" });
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("DB connect."))
   .catch(err => console.error(err));
 
