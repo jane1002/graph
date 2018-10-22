cabbbe0ee622376c398ae32a638e91b0b359c748
Yang Fan <jane.fan1002@gmail.com>
2018-10-22 12:20:54 -0500

    Remove commnets.

--- server.js
+++ server.js
@@ -7,15 +7,9 @@ require("dotenv").config({ path: "variables.env" });
 
 // Bring in GraphQL-Express middleware
 const { ApolloServer } = require("apollo-server-express");
-// const {makeExecutableSchema} = require('graphql-tools');
 const { typeDefs } = require("./schema");
 const { resolvers } = require("./resolvers");
 
-// Create schema
-/* const schema = makeExecutableSchema({
-  typeDefs: typeDefs,
-  resolvers: resolvers, // Or typeDefs, resolvers
-}); */
 const server = new ApolloServer({
   typeDefs: typeDefs,
   resolvers: resolvers,
@@ -29,22 +23,11 @@ const server = new ApolloServer({
 
 const app = express();
 server.applyMiddleware({ app, path: "/graphql" });
+
 // connects to databas
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB connect."))
   .catch(err => console.error(err));
-
-// Create GraphQL application
-/* app.use('/graphiql', graphiqlExpress({endpointUrl: "/graphql"}));
-
-//Connect schemas to GraphQL
-app.use('/graphql', graphqlExpress({
-  schema,
-  context: {
-    Recipe,
-    User,
-  },
-})); */
 
 const PORT = process.env.PORT || 4444;
 
